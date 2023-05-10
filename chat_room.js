544c55a6fd12425369b96c88b99f38f074d07ae7
Mehekk123 <122466056+Mehekk123@users.noreply.github.com>
2023-05-10 16:53:30 +0200

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB-WBB50X8_X1XOm28OEhFUHUzSJtvZqng",
+    authDomain: "kwitter-d8edb.firebaseapp.com",
+    databaseURL: "https://kwitter-d8edb-default-rtdb.firebaseio.com",
+    projectId: "kwitter-d8edb",
+    storageBucket: "kwitter-d8edb.appspot.com",
+    messagingSenderId: "563883804339",
+    appId: "1:563883804339:web:d84f707ffd597b25ded2c6",
+    measurementId: "G-TEGEVY3S47"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
